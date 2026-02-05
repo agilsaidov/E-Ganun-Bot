6e47bf971183babb10591dc45d9b35a23e62c1f5
Aqil Səidov <aqilasa638@gmail.com>
2026-02-05 16:03:44 +0400

    Initial commit

---
+++ src/main/java/com/project/e_ganun/EGanunApplication.java
@@ -0,0 +1,13 @@
+package com.project.e_ganun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EGanunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EGanunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/e_ganun/EGanunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.e_ganun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EGanunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
